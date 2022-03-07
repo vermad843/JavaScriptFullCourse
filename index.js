e605e2f116ec1b25f05c6dd7463d314b8822fb74
vermad843 <vishnuverma7524@gmail.com>
2022-03-07 15:02:53 +0530

    Increment showing on the page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // document.getElementById("count").innerText = 5
 
+let countEl =  document.getElementById("count-el")
+console.log(countEl.innerText);//0
+
 let count = 0;
 
 function increment() {
       count = count + 1;
-     console.log(count)  
+      countEl.innerText = count 
 }
 
 increment();
