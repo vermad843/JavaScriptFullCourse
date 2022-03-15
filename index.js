0358bd97d26ed75e434504f6ca0bd6c25df51316
vermad843 <vishnuverma7524@gmail.com>
2022-03-15 11:10:15 +0530

    multiply

--- index.js
+++ index.js
@@ -178,3 +178,14 @@ function subtract() {
     let result = num1 - num2
     sumEl.textContent = "Sum: " + result 
 }
+
+function divide() {
+    let result = num1/num2
+    sumEl.textContent = "Sum: " + result
+}
+
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
