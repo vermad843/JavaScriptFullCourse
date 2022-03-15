5bd4ee2a3822eadf2f09e9ebcb3351ea7f9022a8
vermad843 <vishnuverma7524@gmail.com>
2022-03-15 10:46:22 +0530

    add function working

--- index.js
+++ index.js
@@ -153,9 +153,23 @@
 let num1 = 8
 let num2 = 2
 
+
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
 // Create four functions : add(), subtract(), divide(), multiply()
-// 
+// Call the correct function when the user clicks on one of the buttons
+// perform the given calculation using num1 and num2
+// render the result of the calculation in the paragraph with id="sum-el"
+
+// e.g if the user clicks on the "Plus" button, you should render
+// "sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let sumEl = document.getElementById('sum-el');
+console.log(sumEl);
+
+function add() {
+  sumEl.textContent = num1 + num2
+  console.log("clicked")
+}
 
